9908fb58fbaf16e242da5ba85d2596ea3cd129a3
dsamyak <you@example.com>
2025-09-15 17:19:13 +0530

    sam

--- make_folders.py
+++ make_folders.py
@@ -1,6 +1,22 @@
 import os
 
-classes = ["अ", "आ", "इ"]   # add every character you need
+classes = [
+"ii",
+"u",
+"uu",
+"e",
+"ai",
+"o",
+"au",
+"k",
+"kh",
+"g",
+"gh",
+"ng",
+"ch",
+"chh",
+"j","jh","ny","tt","tth","dd","ddh","nn","t","th","d","dh","n","p","ph","b","bh","m","y","r","l","v","sh","shh","s","h","ll","ksh","jnya",
+]   # add every character you need
 
 for split in ["train", "val"]:
     for c in classes:
